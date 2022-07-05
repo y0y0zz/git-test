bc18ceb00fe6d4a0efba50311b844f3fa32bc253
y0y0 <y0y0.com>
2022-07-05 22:07:57 +0800

    gitee commit

--- src/main/java/git/GitTest01.java
+++ src/main/java/git/GitTest01.java
@@ -6,13 +6,6 @@ package git;
  */
 public class GitTest01 {
     public static void main(String[] args) {
-        System.out.println("test clone02");
-        System.out.println("猪币吧 这怎么这么菜啊");
-        System.out.println("hot-fix test");
-        System.out.println("insert master");
-        System.out.println("push test");
-        System.out.println("pull test");
-        System.out.println("pull test02 clone");
-        System.out.println("test push gitee");
+        System.out.println("test gitee push");
     }
 }
